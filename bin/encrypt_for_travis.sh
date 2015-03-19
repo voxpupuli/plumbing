1708b7f88e92d17eca76bcea956079b73a018144
Spencer Krum <nibz@spencerkrum.com>
2015-03-19 01:54:03 -0700

    Manage secrets encrypted to travis via script
    Also add a list of modules and the encrypted secrets themselves

---
+++ bin/encrypt_for_travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z ${1} ]; then
+    echo "Usage: $0 <forge password>"
+    exit 1
+fi
+
+forge_password=$1
+travis_args="--skip-version-check --skip-completion-check --no-interactive"
+
+for module in `cat share/modules`
+do
+    echo $module
+    echo -n "secure: "
+    travis encrypt $travis_args -r puppet-community/$module $forge_password
+done
