57ee69cef4a7975c3af26c10cf4da9ee6a3fc8c9
Tim Meusel <tim@bastelfreak.de>
2016-07-02 00:04:07 +0200

    add missing quotes in backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -21,25 +21,25 @@ while true
 do
 
     # clone or update every repo
-    for repo in $(curl -D headers -s ${repos_url} | jq '.[] | .full_name' | tr -d \")
+    for repo in $(curl -D headers -s "${repos_url}" | jq '.[] | .full_name' | tr -d \")
     do
-        repo_short=$(echo ${repo} | cut -d "/" -f 2)
+        repo_short=$(echo "${repo}" | cut -d "/" -f 2)
         echo "${repo_short}"
 
         if ! [ -d "${repo_short}" ]; then
             git clone -q "${git_root}/${repo}"
         fi
-        cd "${repo_short}"
+        cd "${repo_short}" || exit
         git pull -q
-        cd ..
+        cd .. || exit
 
     done
 
     # if there is no next page then we're done and can exit the loop
-    if ! $(cat headers | grep '^Link' | grep 'next' >/dev/null); then
+    if ! grep '^Link' headers | grep 'next' >/dev/null; then
         break
     else
-        next=$(cat headers  | grep '^Link' | cut -d ';' -f 1 | tr -d '<>' | cut -d " " -f 2)
+        next=$(grep '^Link' headers | cut -d ';' -f 1 | tr -d '<>' | cut -d " " -f 2)
     echo Following link: "${next}"
     repos_url="${next}"
     fi
