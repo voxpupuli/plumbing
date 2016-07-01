c17729d5496f73f646a489761ac78bc4ecdd7560
Tim Meusel <tim@bastelfreak.de>
2016-07-02 00:13:04 +0200

    fix shellcheck in forge_push

--- bin/forge_push.sh
+++ bin/forge_push.sh
@@ -2,14 +2,14 @@
 
 module_dir=$1
 
-if [ -z $module_dir ];then
+if [ -z "$module_dir" ];then
     echo Please provide the path to a puppet module
     exit 1
 fi
 
-cd $module_dir
+cd "$module_dir" || exit
 
 rake module:push
 
-cd -
+cd - || exit
 
